ed931942b5f826770a44b6285c6cd0a6eec9f923
xtay <834490071@qq.com>
2019-03-07 16:33:11 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-	//获取arrow元素
+
 	var arrowEl = document.querySelector("#head .headMain > .arrow");
-	//获取所有li元素
+
 	var liNodes = document.querySelectorAll("#head .headMain > .nav > .list > li");
-	//获取所有命名为up的div元素
+
 	var upNodes = document.querySelectorAll("#head .headMain > .nav > .list > li .up");
-	//获取第一个li元素
+
 	var firstLiNode = liNodes[0];
-	//获取第一个li元素的命名为up的div
+
 	var firstUpNode = firstLiNode.querySelector(".up");
 
 	var head = document.querySelector("#head");
@@ -36,8 +36,7 @@ window.onload = function () {
 	//上一屏
 	var preIndex = 0;
 
-	// var music = document.querySelector("#head > .headMain .music");
-	// var audio = document.querySelector("#head > .headMain .music audio");
+
 
 	var mask = document.querySelector("#mask");
 	var line = document.querySelector("#mask .line");
@@ -200,107 +199,7 @@ window.onload = function () {
 	//出入场  结束
 
 	//第五屏气泡效果  开始
-	// biubiu();
-	// function biubiu() {
-	// 	var oc = null;
-	// 	var time1 = 0;
-	// 	var time2 = 0;
-	// 	for (var i = 0; i < team3Lis.length; i++) {
-	// 		team3Lis[i].onmouseenter = function () {//team3Lis绑定事件
-	// 			for (var i = 0; i < team3Lis.length; i++) {
-	// 				team3Lis[i].style.opacity = .3;
-	// 			}
-	// 			this.style.opacity = 1;
-	// 			addCanvas();
-	// 			oc.style.left = this.offsetLeft + "px";
-	// 		}
-	// 	}
 
-	// 	function addCanvas() {
-	// 		if (!oc) {
-	// 			oc = document.createElement("canvas");//在html中创建一个canvas标签
-	// 			oc.width = team3Lis[0].offsetWidth;
-	// 			oc.height = team3Lis[0].offsetHeight * 2 / 3;
-	// 			/*oc.style.background = "pink";*/
-	// 			team3.onmouseleave = function () {//team3Lis绑定事件
-	// 				for (var i = 0; i < team3Lis.length; i++) {
-	// 					team3Lis[i].style.opacity = 1;
-	// 				}
-	// 				removeCanvas();
-	// 			}
-
-	// 			team3.appendChild(oc);
-	// 			QiPao()
-	// 		}
-	// 	}
-
-
-
-	// 	function QiPao() {
-	// 		if (oc.getContext) {
-	// 			var ctx = oc.getContext("2d");
-	// 			var arr = [];
-	// 			//将数组中的圆绘制到画布上
-	// 			time1 = setInterval(function () {
-	// 				ctx.clearRect(0, 0, oc.width, oc.height);
-	// 				//动画
-	// 				for (var i = 0; i < arr.length; i++) {
-	// 					arr[i].deg += 10;
-	// 					arr[i].x = arr[i].startX + Math.sin(arr[i].deg * Math.PI / 180) * arr[i].step * 2;
-	// 					arr[i].y = arr[i].startY - (arr[i].deg * Math.PI / 180) * arr[i].step;
-
-	// 					if (arr[i].y <= 50) {
-	// 						arr.splice(i, 1)
-	// 					}
-	// 				}
-	// 				//绘制
-	// 				for (var i = 0; i < arr.length; i++) {
-	// 					ctx.save();
-	// 					ctx.fillStyle = "rgba(" + arr[i].red + "," + arr[i].green + "," + arr[i].blue + "," + arr[i].alp + ")";
-	// 					ctx.beginPath();
-	// 					ctx.arc(arr[i].x, arr[i].y, arr[i].r, 0, 2 * Math.PI);
-	// 					ctx.fill();
-	// 					ctx.restore();
-	// 				}
-	// 			}, 1000 / 60)
-	// 			//往arr中注入随机圆的信息
-	// 			time2 = setInterval(function () {
-	// 				var r = Math.random() * 6 + 2;
-	// 				var x = Math.random() * oc.width;
-	// 				var y = oc.height - r;
-	// 				var red = Math.round(Math.random() * 255);
-	// 				var green = Math.round(Math.random() * 255);
-	// 				var blue = Math.round(Math.random() * 255);
-	// 				var alp = 1;
-
-	// 				var deg = 0;
-	// 				var startX = x;
-	// 				var startY = y;
-	// 				//曲线的运动形式
-	// 				var step = Math.random() * 20 + 10;
-	// 				arr.push({
-	// 					x: x,
-	// 					y: y,
-	// 					r: r,
-	// 					red: red,
-	// 					green: green,
-	// 					blue: blue,
-	// 					alp: alp,
-	// 					deg: deg,
-	// 					startX: startX,
-	// 					startY: startY,
-	// 					step: step
-	// 				})
-	// 			}, 50)
-	// 		}
-	// 	}
-	// 	function removeCanvas() {
-	// 		oc.remove();
-	// 		oc = null;
-	// 		clearInterval(time1);
-	// 		clearInterval(time2);
-	// 	}
-	// }
 
 	//第五屏气泡效果  结束
 
@@ -608,4 +507,19 @@ window.onload = function () {
 			anArr[preIndex]["outAn"]();
 		}
 	}
+
+
+	var bimg = document.querySelector(".box-img");
+	var box = document.querySelector(".box");
+	var bcon = document.querySelector(".box-content");
+	box.addEventListener("mouseover",function(){
+		bimg.style.transform="rotateY(90deg)";
+		bcon.style.opacity="0.8";
+		bcon.style.transform="rotateY(0deg)";
+	})
+	box.addEventListener("mouseout",function(){
+		bimg.style.transform="rotateY(0deg)";
+		bcon.style.opacity="0";
+		bcon.style.transform="rotateY(90deg)";
+	})
 }
